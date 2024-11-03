30019289539038a3f2d1a5c9ec344ed4280362c4
TangRuixuan <60222217+TangRuixuan@users.noreply.github.com>
2024-11-03 12:06:41 +0800

    Add files via upload

--- Appointment.java
+++ Appointment.java
@@ -1,8 +1,30 @@
-public interface Appointment {
 
-    public void checkAppointment();
-    public void updateAppointment();
-
-
-    
-} 
+public class Appointment {
+private String Patient;
+private String Doctor;
+private String Date;
+private String Time;
+private String Status;
+public Appointment(String Patient, String Doctor, String Date, String Time, String status) {
+	this.Patient=Patient;
+	this.Doctor=Doctor;
+	this.Date=Date;
+	this.Time=Time;
+	this.Status=status;
+}
+public String getPatient() {
+	return Patient;
+}
+public String getDoctor() {
+	return Doctor;
+}
+public String getDate() {
+	return Date;
+}
+public String getTime() {
+	return Time;
+}
+public String getStatus() {
+	return Status;
+}
+}
